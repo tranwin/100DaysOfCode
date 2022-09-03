623e317a33311babac1351e09aa13bd5a8959c7e
Tran Nguyen <ngtrannguyen@gmail.com>
2022-09-03 15:38:43 +0700

    Final touch for Ceasar Cipher app

--- CeasarCipher/main.py
+++ CeasarCipher/main.py
@@ -3,10 +3,6 @@ print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number that is lower than 30:\n"))
-
 # Create encrypt function
 def decipher(start_text,shift_amount,cipher_direction):
     end_text = ""
@@ -23,6 +19,18 @@ def decipher(start_text,shift_amount,cipher_direction):
             end_text += letter
         
     print(f"The {cipher_direction}d text is {end_text}")
-            
-shift = shift %26
-decipher(start_text=text,shift_amount=shift,cipher_direction=direction)
+
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number that is lower than 30:\n"))
+
+    shift = shift %26
+    decipher(start_text=text,shift_amount=shift,cipher_direction=direction)
+
+    #Enhance user experience by asking if you want to go again
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye!")
