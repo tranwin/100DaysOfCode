7afa8eb844332f4a43a727f554c5a7f0fbbb72d7
tnguyen <ngtrannguyen@gmail.com>
2022-11-14 13:17:38 -0800

    .

---
+++ 5-Password-generator/main.py
@@ -0,0 +1,25 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+
+# Generate list of characters
+list_letters = random.sample(letters, nr_letters)
+list_numbers = random.sample(numbers,nr_numbers)
+list_symbols = random.sample(symbols, nr_symbols)
+
+# Generate final character pool
+nr_password = nr_letters + nr_numbers + nr_symbols
+list_password = list_letters + list_numbers + list_symbols
+
+#Print out password
+password = random.sample(list_password,nr_password)
+print("Your password is:",''.join(password))
+
+
